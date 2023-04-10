3ca63d9c96ae46a28ea38b3ecb4a4d3a20a813a3
NGAM TARYLL <taryllngam@gmail.com>
2023-04-10 23:32:25 +0100

    adding functionallity with js

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ const str2 = document.getElementById('str2')
 const btn = document.getElementById('btn')
 
 
-const isIsomorphic = (str1,str2) => {
-  if(str1.length !== str2.length){
+function isIsomorphic(str1, str2) {
+  if (str1.length != str2.length) {
     return false;
-  };
-  for(let i = 0; i < str1.length; i++){
-    index1 = str1.indexof(str1[i]);
-    index2 = str2.indexof(str2[i]);
-    if(str1(index2) != str1[i]){
-      str2(index1) != str2[i]
-        return false
+  }
+  let map = new Map();
+  let set = new Set();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(str1[i])) {
+      if (map.get(str2[i]) != t[i]) {
+        return false;
+      }
     }
-  }return true
+    else {
+      if (set.has(t[i])) {
+        return false;
+      }
+      map.set(s[i], t[i]);
+      set.add(t[i]);
+    }
+  }
+  return true;
 }
 btn.addEventListener('click', isIsomorphic)
 
